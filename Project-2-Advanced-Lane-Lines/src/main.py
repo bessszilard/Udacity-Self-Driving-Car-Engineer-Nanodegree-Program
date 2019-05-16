f1d1823d620a0951d1a7fce46a5135b219eac119
Szilard-Bessenyei <bessszilard@gmail.com>
2019-05-16 13:55:19 +0200

    HLS filtering implemented with trackbar adjustment
    GUI for adjusting HLS parameters. 6 image is combined to one

--- Project-2-Advanced-Lane-Lines/src/main.py
+++ Project-2-Advanced-Lane-Lines/src/main.py
@@ -9,43 +9,49 @@ import cv2
 from cameraCalibration import get_undistorted_image
 from combiningThresholds import abs_sobel_thresh, abs_sobel_thresh, mag_thresh, dir_threshold, mag_dir_treshold
 
-# 54       255     -1.4486232791552935     1.3089969389957472
-sobelMagMin = 54
-sobelMagMax = 255
-sobelAngMin = -1.4486232791552935
-sobelAngMax = 1.3089969389957472
+h_ch = [  3,  31] 
+l_ch = [  0, 255]
+s_ch = [110, 255]
+# h_ch = [0, 180]
+# l_ch = [0, 255]
+# s_ch  = [0, 255]
 
-def on_sobel_mag_min_trackbar(val):
-    global sobelMagMin
-    sobelMagMin = val
-    res = mag_dir_treshold(image, sobel_kernel=ksize, mag_thresh=(sobelMagMin, sobelMagMax),
-                                 dir_thresh = (sobelAngMin, sobelAngMax))
-    cv2.imshow(sobel_window_name, res )
-    print(sobelMagMin, "\t", sobelMagMax, "\t", sobelAngMin, "\t", sobelAngMax)
+def filter_and_show(image, h, l, s):
+    low_th = np.array([h[0], l[0], s[0]])
+    high_th = np.array([h[1], l[1], s[1]])
+    res = cv2.inRange(image, low_th, high_th)
+    cv2.imshow(hls_window_name, res )
 
-def on_sobel_mag_max_trackbar(val):
-    global sobelMagMax
-    sobelMagMax = val
-    res = mag_dir_treshold(image, sobel_kernel=ksize, mag_thresh=(sobelMagMin, sobelMagMax),
-                                 dir_thresh = (sobelAngMin, sobelAngMax))
-    cv2.imshow(sobel_window_name, res )  
-    print(sobelMagMin, "\t", sobelMagMax, "\t", sobelAngMin, "\t", sobelAngMax)
+def hls_h_min_ch_trackbar(val):
+    global h_ch
+    h_ch[0] = val
+    filter_and_show(image, h_ch, l_ch, s_ch)
 
-def on_sobel_ang_min_trackbar(val):
-    global sobelAngMin
-    sobelAngMin = np.radians(val - 90)
-    res = mag_dir_treshold(image, sobel_kernel=ksize, mag_thresh=(sobelMagMin, sobelMagMax),
-                                 dir_thresh=(sobelAngMin, sobelAngMax))
-    cv2.imshow(sobel_window_name, res )  
-    print(sobelMagMin, "\t", sobelMagMax, "\t", sobelAngMin, "\t", sobelAngMax)
+def hls_h_max_ch_trackbar(val):
+    global h_ch
+    h_ch[1] = val
+    filter_and_show(image, h_ch, l_ch, s_ch)
 
-def on_sobel_ang_max_trackbar(val):
-    global sobelAngMax
-    sobelAngMax = np.radians(val - 90)
-    res = mag_dir_treshold(image, sobel_kernel=ksize, mag_thresh=(sobelMagMin, sobelMagMax),
-                                 dir_thresh=(sobelAngMin, sobelAngMax))
-    cv2.imshow(sobel_window_name, res )  
-    print(sobelMagMin, "\t", sobelMagMax, "\t", sobelAngMin, "\t", sobelAngMax)
+def hls_l_min_ch_trackbar(val):
+    global l_ch
+    l_ch[0] = val
+    filter_and_show(image, h_ch, l_ch, s_ch)
+
+def hls_l_max_ch_trackbar(val):
+    global l_ch
+    l_ch[1] = val
+    filter_and_show(image, h_ch, l_ch, s_ch)
+
+def hls_s_min_ch_trackbar(val):
+    global s_ch
+    s_ch[0] = val
+    filter_and_show(image, h_ch, l_ch, s_ch)
+
+def hls_s_max_ch_trackbar(val):
+    global s_ch
+    s_ch[1] = val
+    filter_and_show(image, h_ch, l_ch, s_ch)
+
 
 # calculated already with cameraCalibration.py
 cameraMx = np.array([[1.15660712e+03, 0.00000000e+00, 6.68960302e+02],
@@ -57,7 +63,6 @@ images_file_names = glob.glob('test_images/test*.jpg')
 
 rows = 2
 cols = 3
-
 
 disp_imgRow1 = mpimg.imread(images_file_names[0])
 
@@ -76,58 +81,37 @@ for i in range(4, len(images_file_names)):
 
 image = np.concatenate((disp_imgRow1, disp_imgRow2), axis=0)
 
+image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
 # plt.imshow(image)
 # plt.show()
 
 # image = mpimg.imread('test_images/test2.jpg')
 image = cv2.blur = cv2.blur(image, (5,5))
-# image = get_undistorted_image(image, cameraMx, distCoeffs)
 
-ksize = 5 # Choose a larger odd number to smooth gradient measurements
-# gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(30, 255))
-# grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(30, 255))
-# mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(20, 200))
-# dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=( np.radians(45), np.pi/2))
+hls_window_name = "HLS filter"
+cv2.namedWindow(hls_window_name, cv2.WINDOW_NORMAL)
+trackbarHMinName = "H min"
+trackbarHMaxName = "H max"
+trackbarLMinName = "L min"
+trackbarLMaxName = "L max"
+trackbarsMinName = "S min"
+trackbarsMaxName = "S max"
 
-sobel_window_name = "Sobel mag"
+cv2.createTrackbar(trackbarHMinName, hls_window_name, 0, 180, hls_h_min_ch_trackbar)
+cv2.createTrackbar(trackbarHMaxName, hls_window_name, 0, 180, hls_h_max_ch_trackbar)
 
-cv2.namedWindow(sobel_window_name, cv2.WINDOW_NORMAL)
+cv2.createTrackbar(trackbarLMinName, hls_window_name, 0, 255, hls_l_min_ch_trackbar)
+cv2.createTrackbar(trackbarLMaxName, hls_window_name, 0, 255, hls_l_max_ch_trackbar)
 
-# desiredWidth = 640
-# desiredheight = 480
-# cv2.resizewindow(sobel_window_name, desiredWidth,desiredheight)
+cv2.createTrackbar(trackbarsMinName, hls_window_name, 0, 255, hls_s_min_ch_trackbar)
+cv2.createTrackbar(trackbarsMaxName, hls_window_name, 0, 255, hls_s_max_ch_trackbar)
 
-trackbarMagMinName = "Min mag"
-trackbarMagMaxName = "Max mag"
-trackbarAngMinName = "Min angle"
-trackbarAngMaxName = "Max angle"
+cv2.setTrackbarPos(trackbarHMinName, hls_window_name, h_ch[0] )
+cv2.setTrackbarPos(trackbarHMaxName, hls_window_name, h_ch[1] )
+cv2.setTrackbarPos(trackbarLMinName, hls_window_name, l_ch[0] )
+cv2.setTrackbarPos(trackbarLMaxName, hls_window_name, l_ch[1] )
+cv2.setTrackbarPos(trackbarsMinName, hls_window_name, s_ch[0] )
+cv2.setTrackbarPos(trackbarsMaxName, hls_window_name, s_ch[1] )
 
-cv2.createTrackbar(trackbarMagMinName, sobel_window_name, 0, 255, on_sobel_mag_min_trackbar)
-cv2.createTrackbar(trackbarMagMaxName, sobel_window_name, 0, 255, on_sobel_mag_max_trackbar)
 
-cv2.createTrackbar(trackbarAngMinName, sobel_window_name, 0, 180, on_sobel_ang_min_trackbar)
-cv2.createTrackbar(trackbarAngMaxName, sobel_window_name, 0, 180, on_sobel_ang_max_trackbar)
-
-cv2.setTrackbarPos(trackbarMagMinName, sobel_window_name, sobelMagMin )
-cv2.setTrackbarPos(trackbarMagMaxName, sobel_window_name, sobelMagMax )
-cv2.setTrackbarPos(trackbarAngMinName, sobel_window_name, int(sobelAngMin * 180 / np.pi + 90) )
-cv2.setTrackbarPos(trackbarAngMaxName, sobel_window_name, int(sobelAngMax * 180 / np.pi + 90) )
-
-# on_sobel_mag_min_trackbar(0)
-# on_sobel_mag_max_trackbar(255)
-
-# f, (ax) = plt.subplots(2, 3, figsize=(24, 5))
-# f.tight_layout()
-# ax[0][0].imshow(image)
-# ax[0][0].set_title('Original Image', fontsize=10)
-# ax[0][1].imshow(gradx, cmap='gray')
-# ax[0][1].set_title('Thresholded Gradient X direction', fontsize=10)
-# ax[0][2].imshow(grady, cmap='gray')
-# ax[0][2].set_title('Thresholded Gradient Y direction', fontsize=10)
-# ax[1][0].imshow(mag_binary, cmap='gray')
-# ax[1][0].set_title('Threshold magnitude', fontsize=10)
-# ax[1][1].imshow(dir_binary, cmap='gray')
-# ax[1][1].set_title('Directions of the Gradient', fontsize=10)
-
-# plt.show()
 cv2.waitKey(0)
