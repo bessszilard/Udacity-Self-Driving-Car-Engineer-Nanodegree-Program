72bd29fb85109c004cb3f12651184cacd56ddbbb
Szilard-Bessenyei <bessszilard@gmail.com>
2019-06-24 15:47:58 +0200

    P8 - Add PID controller project
    Repo sorce: https://github.com/udacity/CarND-PID-Control-Project

---
+++ Project-8-PID-Controller/src/PID.cpp
@@ -0,0 +1,30 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return 0.0;  // TODO: Add your total error calc here!
+}
