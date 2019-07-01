65803aa23d8837d13d6176fc95bd7a2a35d4b37a
Szilard Bessenyei <besszilard@gmail.com>
2019-07-01 13:13:44 +0100

    P7: Add Frenet coordinates
    The car follows it's line.

--- Project-7-Path-Planning-Project/src/helpers.h
+++ Project-7-Path-Planning-Project/src/helpers.h
@@ -40,6 +40,7 @@ double distance(double x1, double y1, double x2, double y2) {
 }
 
 // Calculate closest waypoint to current x, y position
+
 int ClosestWaypoint(double x, double y, const vector<double> &maps_x, 
                     const vector<double> &maps_y) {
   double closestLen = 100000; //large number
@@ -59,6 +60,10 @@ int ClosestWaypoint(double x, double y, const vector<double> &maps_x,
 }
 
 // Returns next waypoint of the closest waypoint
+/**
+ * check which is the next waypoint
+ *
+ */ 
 int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, 
                  const vector<double> &maps_y) {
   int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
@@ -82,6 +87,10 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+/**
+ * x, y, theta changes
+ * map_x and map_y is determiend at the beginning of the process
+ */
 vector<double> getFrenet(double x, double y, double theta, 
                          const vector<double> &maps_x, 
                          const vector<double> &maps_y) {
@@ -127,6 +136,11 @@ vector<double> getFrenet(double x, double y, double theta,
 }
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
+/**
+ * Frebet -> Cartesian
+ * s, d, changes,
+ * map_ is determined in the beginning
+ */
 vector<double> getXY(double s, double d, const vector<double> &maps_s, 
                      const vector<double> &maps_x, 
                      const vector<double> &maps_y) {
